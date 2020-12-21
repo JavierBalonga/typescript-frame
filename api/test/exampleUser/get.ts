782615f168c9d0e0f358c22c2f6a34378973aeb9
JavierBalonga <56722632+JavierBalonga@users.noreply.github.com>
2020-12-21 19:49:49 -0300

    test-ci

--- api/test/exampleUser/get.ts
+++ api/test/exampleUser/get.ts
@@ -5,19 +5,13 @@ import Server from '../../server';
 import db from '../../server/models';
 
 describe('GET /api/v1/exampleUser/', () => {
-  beforeEach(() => {
-    // ...
-  });
 
   it('should get all example users', () =>
     request(Server)
       .get('/api/v1/exampleUser')
       .expect('Content-Type', /json/)
       .then((r) => {
-        console.log(r.body);
+        expect(r.body).to.be.an('array').that.is.not.empty;
       }));
 
-  afterEach(() => {
-    // ...
-  });
 });
