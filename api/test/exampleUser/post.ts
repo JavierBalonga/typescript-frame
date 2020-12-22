32d7d42ec84e30dabb7c31d0032e7c245dec8776
JavierBalonga <56722632+JavierBalonga@users.noreply.github.com>
2020-12-21 22:44:42 -0300

    idk

--- api/test/exampleUser/post.ts
+++ api/test/exampleUser/post.ts
@@ -11,7 +11,7 @@ describe('POST /api/v1/exampleUser/', () => {
       .send({ firstName: 'test', lastName: 'user' })
       .expect('Content-Type', /json/)
       .then((r) => {
-        console.log(r.body)
+        console.log(r.body.errors)
         expect(r.body).to.be.an('array').that.is.not.empty;
         return db.ExampleUser.findOne({
           where: { firstName: 'test', lastName: 'user' },
