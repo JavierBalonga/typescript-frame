c46775afc8cdca95d82e088c77dd021c243be8c9
JavierBalonga <56722632+JavierBalonga@users.noreply.github.com>
2020-12-22 15:34:16 -0300

    admin user

--- api/db/migrations/20201221132756-create-exampleUser.js
+++ api/db/migrations/20201221132756-create-exampleUser.js
@@ -1,30 +1,51 @@
 'use strict';
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('example_users', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      first_name: {
-        type: Sequelize.STRING,
-      },
-      last_name: {
-        type: Sequelize.STRING,
-      },
-      created_at: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updated_at: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-    });
+  up: (queryInterface, Sequelize) => {
+    return queryInterface
+      .createTable('example_users', {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        first_name: {
+          type: Sequelize.STRING,
+        },
+        last_name: {
+          type: Sequelize.STRING,
+        },
+        created_at: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+        updated_at: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+      })
+      .then(() =>
+        queryInterface.bulkInsert(
+          'example_users',
+          [
+            {
+              id: 1,
+              first_name: 'admin',
+              last_name: 'admin',
+              created_at: new Date(),
+              updated_at: new Date(),
+            },
+          ],
+          { returning: true }
+        )
+      )
+      .then((r) =>
+        queryInterface.sequelize.query(
+          `ALTER SEQUENCE example_users_id_seq RESTART WITH ${r.length + 1}`
+        )
+      );
   },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('example_users');
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('example_users');
   },
 };
